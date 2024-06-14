df0799811f49b4c21ce02ee7841a3bf050686260
Marton Takacs <marton.takacs@tesco.com>
2024-06-14 13:09:04 +0200

    return input from request

--- src/main/java/org/example/CloudFunction.java
+++ src/main/java/org/example/CloudFunction.java
@@ -3,10 +3,16 @@ package org.example;
 import com.google.cloud.functions.HttpFunction;
 import com.google.cloud.functions.HttpRequest;
 import com.google.cloud.functions.HttpResponse;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 public class CloudFunction implements HttpFunction {
+    private static final Gson gson = new Gson();
+
     @Override
     public void service(HttpRequest httpRequest, HttpResponse httpResponse) throws Exception {
-        httpResponse.getWriter().write("Jeeeeeeeej muxik a function!");
+        JsonObject body = gson.fromJson(httpRequest.getReader(), JsonObject.class);
+        String inputMessage = body.get("message").getAsString();
+        httpResponse.getWriter().write("Jeeeeeeeej muxik a function! Input message: " + inputMessage);
     }
 }
